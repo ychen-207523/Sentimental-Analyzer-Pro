d2a2184a765f21dcc373a51329502ec06f77d501
Prasanth Yadla <pyadla2@ncsu.edu>
2020-10-27 09:16:54 -0400

    Update test cases for audio backend

--- test/test_sentiment_analysis.py
+++ test/test_sentiment_analysis.py
@@ -1,9 +1,26 @@
-from pydoc import text
-import sentimental_analysis.audio.audio_analyzer
+import sys
+sys.path.append("../sentimental_analysis/audio/")
+import unittest
+import audio_analyzer
+
+# Unit Test Case for Audio Sentiment Analyzer
+class AudioSentimentAnalyzerTestCase(unittest.TestCase):
+    
+    # Setup
+    def setup(self):
+        self.aa = audio_analyzer.AudioAnalyzer()
+
+    # Test case for speech_to_text method
+    def test_speech_to_text(self):
+        aa = audio_analyzer.AudioAnalyzer()
+        self.assertEqual(aa.speech_to_text("test_wv.wav"), "hello how are you")
+
+    # Test case for sentiment_analyzer_scores method
+    def test_sentiment_analyzer_scores(self):
+        aa = audio_analyzer.AudioAnalyzer()
+        self.assertEqual(aa.sentiment_analyzer_scores("hello how are you")["pos"], 0)
 
 
-def speech_to_text_test():
-    pass
-
-def sentiment_analyzer_scores_test(analyser=None):
-    assert score==analyser.polarity_scores(sentence)
+# main function
+if __name__ == '__main__':
+     unittest.main()
