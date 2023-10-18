4905d3cff0591a174270d291d964e9463f93c3fd
Sohamkumar Patel <sohampatel1117@gmail.com>
2023-10-18 16:27:22 -0400

    Added analyzed text area in result.html

--- sentimental_analysis/realworld/utilityFunctions.py
+++ sentimental_analysis/realworld/utilityFunctions.py
@@ -24,11 +24,6 @@ def removeSpecialChar(text):
 
 
 def sentiment_scores(sentence):
-    # Create a SentimentIntensityAnalyzer object.
     sid_obj = SentimentIntensityAnalyzer()
-
-    # polarity_scores method of SentimentIntensityAnalyzer
-    # object gives a sentiment dictionary.
-    # which contains pos, neg, neu, and compound scores.
     sentiment_dict = sid_obj.polarity_scores(sentence)
     return sentiment_dict
