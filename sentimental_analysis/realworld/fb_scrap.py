3a12463bef0f05fb453afeef7047d159529cf22e
Yunfei <ychen207523@gmail.com>
2024-10-08 23:32:01 -0400

    update module

--- sentimental_analysis/realworld/fb_scrap.py
+++ sentimental_analysis/realworld/fb_scrap.py
@@ -4,10 +4,10 @@ import pandas as pd
 import numpy as np
 import os
 from sklearn.feature_extraction.text import CountVectorizer
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import Sequential
-from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
 import pandas as pd
