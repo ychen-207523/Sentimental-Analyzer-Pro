b05dff112fa6b4010d1d2ecafeb5b41efd0f4637
ShubhamTidke <shubhamvt2000@gmail.com>
2024-10-28 13:37:09 -0400

    <Project 2> | ShubhamTidke | Added summary of news articles to news.json

--- sentimental_analysis/realworld/newsScraper.py
+++ sentimental_analysis/realworld/newsScraper.py
@@ -7,24 +7,22 @@ user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 
 def scrapNews(topicName):
     article_list = []
-    response = getNewsData(topicName)
-    results = response.json().get("items", [])
+    news_results = getNewsResults(topicName)
+    config = Config()
+    config.browser_user_agent = user_agent
 
-    for result in results:
-        link = result.get("link")
-
-        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
-        config = Config()
-        config.browser_user_agent = user_agent
-
-        article = Article(link, config=config)
+    for url in news_results:
+        article = Article(url, config=config, language="en")
         article.download()
         article.parse()
+
+        # NLP on the article
         article.nlp()
         dict = {}
-        if article.summary[:2] != "Ad":
-            dict['Summary'] = article.summary
-            article_list.append(dict)
+
+        # Extract summary
+        dict['Summary'] = article.summary
+        article_list.append(dict)
 
     with open('sentimental_analysis/realworld/news.json', 'w') as json_file:
         json.dump(article_list, json_file)
@@ -37,7 +35,7 @@ def getNewsResults(query):
         "User-Agent":user_agent
     }
 
-    base_url = f"https://www.google.com/search?q={query}&gl=us&tbm=nws&num=100"
+    base_url = f"https://www.google.com/search?q={query}&gl=us&tbm=nws&num=10"
     response = requests.get(base_url, headers=headers)
     soup = BeautifulSoup(response.content, "html.parser")
     news_results = []
