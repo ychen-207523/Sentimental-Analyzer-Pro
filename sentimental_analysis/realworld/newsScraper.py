122864a682693b0da7c8695e57bfa65929d5c918
ShubhamTidke <shubhamvt2000@gmail.com>
2024-10-28 14:12:01 -0400

    <Project 2> | ShubhamTidke | Added comments and formatted the file

--- sentimental_analysis/realworld/newsScraper.py
+++ sentimental_analysis/realworld/newsScraper.py
@@ -6,6 +6,8 @@ import logging
 
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"
 
+
+# This method scraps article using google news that match the relevant query and dumps it on news.json file
 def scrapNews(topicName):
     article_list = []
     news_results = getNewsResults(topicName)
@@ -14,37 +16,38 @@ def scrapNews(topicName):
     count = 10
 
     for url in news_results:
-      if count == 0:
-        break
-      try:
-          article = Article(url, config=config, language="en")
-          article.download()
-          article.parse()
+        if count == 0:
+            break
+        try:
+            article = Article(url, config=config, language="en")
+            article.download()
+            article.parse()
 
-          # NLP on the article
-          article.nlp()
-          dict = {}
+            # NLP on the article
+            article.nlp()
+            dict = {}
 
-          # Extract summary
-          if (article.summary !=  "Please enable JS and disable any ad blocker"):
-            dict['Summary'] = article.summary
-            article_list.append(dict)
-            count -= 1
-          else:
-             logging.info(f"Skipping article as it requires JS - {url}")
-      except BaseException as e:
-          logging.info(f"Error occured while extracting summary of article - {url}\nError - {e}")
+            # Extract summary
+            if article.summary != "Please enable JS and disable any ad blocker":
+                dict["Summary"] = article.summary
+                article_list.append(dict)
+                count -= 1
+            else:
+                logging.info(f"Skipping article as it requires JS - {url}")
+        except BaseException as e:
+            logging.info(
+                f"Error occured while extracting summary of article - {url}\nError - {e}"
+            )
 
-    with open('sentimental_analysis/realworld/news.json', 'w') as json_file:
+    with open("sentimental_analysis/realworld/news.json", "w") as json_file:
         json.dump(article_list, json_file)
 
     logging.warning("Articles saved to news.json")
 
+
 # This method returns URLs to news websites matching the relevant query
 def getNewsResults(query):
-    headers = {
-        "User-Agent":user_agent
-    }
+    headers = {"User-Agent": user_agent}
 
     base_url = f"https://www.google.com/search?q={query}&gl=us&tbm=nws&num=100"
     response = requests.get(base_url, headers=headers)
